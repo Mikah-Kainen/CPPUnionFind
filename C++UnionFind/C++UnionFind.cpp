a68c4aa1000ac780bf9477d21e0bbdf649bd411b
Mikah <mikah@mltd.com>
2023-01-09 16:37:10 -0800

    Optimized Quick Union

--- C++UnionFind/C++UnionFind.cpp
+++ C++UnionFind/C++UnionFind.cpp
@@ -3,25 +3,73 @@
 
 #include <iostream>
 #include "UnionFind.cpp"
+#include <random>
+#include <chrono>
 
 int main()
 {
     while (true)
     {
-        std::string values[] = { "0", "1", "2", "3", "4", "5", };
-        QuickFind<std::string> quickFind = QuickFind<std::string>(values, 6);
+        //std::string values[] = { "0", "1", "2", "3", "4", "5", };
+        //QuickFind<std::string> quickFind = QuickFind<std::string>(values, 6);
 
-        bool result1 = quickFind.Union("1", "2"); //true
-        bool result2 = quickFind.AreConnected("1", "5"); //false
-        bool result3 = quickFind.Union("2", "5"); //true
-        bool result4 = quickFind.AreConnected("1", "5"); //true
-        int result5 = quickFind.Find("3"); // 3
-        int result6 = quickFind.Find("2"); // 5
-        bool result7 = quickFind.Union("0", "4"); //true
-        bool result8 = quickFind.AreConnected("4", "5"); //false
-        bool result9 = quickFind.Union("1", "2"); //false
-        bool result10 = quickFind.Union("1", "6"); //false
-        int result11 = quickFind.Find("20"); // -1
+        //bool result1 = quickFind.Union("1", "2"); //true
+        //bool result2 = quickFind.AreConnected("1", "5"); //false
+        //bool result3 = quickFind.Union("2", "5"); //true
+        //bool result4 = quickFind.AreConnected("1", "5"); //true
+        //int result5 = quickFind.Find("3"); // 3
+        //int result6 = quickFind.Find("2"); // 5
+        //bool result7 = quickFind.Union("0", "4"); //true
+        //bool result8 = quickFind.AreConnected("4", "5"); //false
+        //bool result9 = quickFind.Union("1", "2"); //false
+        //bool result10 = quickFind.Union("1", "6"); //false
+        //int result11 = quickFind.Find("20"); // -1
+
+
+
+        int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int length = 10;
+        QuickUnion<int> quickUnion = QuickUnion<int>(values, length);
+        quickUnion.Union(0, 1);
+        quickUnion.Union(2, 3);
+        quickUnion.Union(4, 5);
+        quickUnion.Union(6, 7);
+        quickUnion.Union(8, 9);
+        quickUnion.Union(1, 2);
+        quickUnion.Union(5, 6);
+        quickUnion.Union(2, 5);
+        quickUnion.Union(1, 9);
+
+        //const int length = 200;
+        //int values[length];
+        //for (int i = 0; i < length; i ++)
+        //{
+        //    values[i] = i;
+        //}
+        //QuickUnion<int> quickUnion = QuickUnion<int>(values, length);
+
+        //for (int i = 2; i < length -2; i += 3)
+        //{
+        //    quickUnion.Union(i, i - 1);
+        //    quickUnion.Union(i - 1, i + 1);
+        //    quickUnion.Union(i - 2, i + 2);
+        //}
+
+        //std::srand(std::chrono::system_clock::now().time_since_epoch().count());
+        //for (int i = 0; i < 25; i ++)
+        //{
+        //    quickUnion.Union(std::rand() % length, std::rand() % length);
+        //}
+
+        int deepestDepth = 0;
+        for (int i = 0; i < length; i ++)
+        {
+            std::pair<int, int> result = quickUnion.Find(i);
+            if (result.second > deepestDepth)
+            {
+                deepestDepth = result.second;
+            }
+        }
     }
 }
 
--- C++UnionFind/UnionFind.cpp
+++ C++UnionFind/UnionFind.cpp
@@ -5,7 +5,7 @@ template <typename T>
 class QuickFind
 {
 private:
-	int* sets; 
+	std::pair<int, int>* sets;
 	std::unordered_map<T, int> dictionary;
 	int length;
 
@@ -13,21 +13,21 @@ public:
 	QuickFind(T values[], int length)
 	{
 		this->length = length;
-		sets = new int[length];
+		sets = new std::pair<int, int>[length];
 		dictionary = std::unordered_map<T, int>();
-		for (int i = 0; i < length ; i ++)
+		for (int i = 0; i < length; i++)
 		{
 			dictionary.insert(std::pair<T, int>(values[i], i));
-			sets[i] = i;
+			sets[i] = std::pair<int, int>(i, 0);
 		}
 	}
 
 	~QuickFind()
 	{
-		delete sets;
+		delete[] sets;
 	}
 
-	int Find(T value)
+	std::pair<int, int> Find(T value)
 	{
 		if (!dictionary.contains(value))
 		{
@@ -36,14 +36,14 @@ public:
 		return sets[dictionary[value]];
 	}
 
-	bool Union(T p, T q) 
+	bool Union(T p, T q)
 	{
 		if (!dictionary.contains(p) || !dictionary.contains(q))
 		{
 			return false;
 		}
-		int oldSet = Find(p);
-		int newSet = Find(q);
+		std::pair<int, int> oldSet = Find(p);
+		std::pair<int, int> newSet = Find(q);
 		if (oldSet == newSet)
 		{
 			return false;
@@ -80,7 +80,7 @@ template <typename T>
 class QuickUnion
 {
 private:
-	int* sets;
+	std::pair<int, int>* sets;
 	std::unordered_map<T, int> dictionary;
 	int length;
 
@@ -88,40 +88,33 @@ public:
 	QuickUnion(T values[], int length)
 	{
 		this->length = length;
-		sets = new int[length];
+		sets = new std::pair<int, int>[length];
 		dictionary = std::unordered_map<T, int>();
 		for (int i = 0; i < length; i++)
 		{
 			dictionary.insert(std::pair<T, int>(values[i], i));
-			sets[i] = i;
+			sets[i] = std::pair<int, int>(i, 0);
 		}
 	}
 
 	~QuickUnion()
 	{
-		delete sets;
+		delete[] sets;
 	}
 
-	int Find(T value, int& depth)
+	std::pair<int, int> Find(T value)
 	{
 		if (!dictionary.contains(value))
 		{
-			return -1;
+			return std::pair<int, int>(-1, -1);
 		}
-		depth = 0;
-		int currentIndex = dictionary[value];
-		while (sets[currentIndex] != currentIndex)
-		{
-			depth++;
-			currentIndex = sets[currentIndex];
-		}
-		return currentIndex;
-	}
 
-	int Find(T value)
-	{
-		int bla;
-		return Find(value, bla);
+		int currentIndex = dictionary[value];
+		while (sets[currentIndex].first != currentIndex)
+		{
+			currentIndex = sets[currentIndex].first;
+		}
+		return sets[currentIndex];
 	}
 
 
@@ -131,30 +124,29 @@ public:
 		{
 			return false;
 		}
-		
-		int depth1;
-		int set1 = Find(p, depth1);
-		int depth2;
-		int set2 = Find(q, depth2);
+
+		std::pair<int, int> set1 = Find(p);
+		std::pair<int, int> set2 = Find(q);
 		if (set1 == set2)
 		{
 			return false;
 		}
 
-		int newSet;
-		int oldSet;
-		if (depth1 < depth2)
+		if (set1.second == set2.second)
 		{
-			newSet = set1;
-			oldSet = set2;
+			set2 = std::pair<int, int>(set2.first, set2.second + 1);
+			sets[set2.first] = set2;
+			sets[set1.first] = set2;
+		}
+		else if (set1.second > set2.second)
+		{
+			sets[set2.first] = set1;
 		}
 		else
 		{
-			newSet = set2;
-			oldSet = set1;
+			sets[set1.first] = set2;
 		}
 
-		sets[oldSet] = newSet;
 		return true;
 	}
 
